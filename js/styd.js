ae41246bf8f3b12e3d036adeae23b51140645580
btinfo <btchinainfo@gmail.com>
2021-08-03 02:46:29 +0800

    20210803@02:46

--- js/styd.js
+++
@@ -1,7 +0,0 @@
-var body = $response.body;
-var obj = JSON.parse(body);
-obj.data["vvip"] = "1";
-obj.data["vvip_deadline"] = "1835312949000";
-obj.data["u_credits"] = "666666";
-body = JSON.stringify(obj);
-$done({body});
