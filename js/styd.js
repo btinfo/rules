5594e723555f70b9efb19f2accf48c76c82b39e0
btinfo <btchinainfo@gmail.com>
2021-09-04 12:17:22 +0800

    20210904@12:17

---
+++ js/styd.js
@@ -0,0 +1,7 @@
+var body = $response.body;
+var obj = JSON.parse(body);
+obj.data["vvip"] = "1";
+obj.data["vvip_deadline"] = "1835312949000";
+obj.data["u_credits"] = "666666";
+body = JSON.stringify(obj);
+$done({body});
---
+++ js/wnyd.js
@@ -0,0 +1,5 @@
+var body = $response.body;
+var obj = JSON.parse(body);
+obj.tradeEndTime = 1679685290000;
+body = JSON.stringify(obj);
+$done({body});
