5fb53d061cdf036bf51ab4a920d1a69892b725f7
btinfo <btchinainfo@gmail.com>
2021-10-12 15:59:30 +0800

    20211012@15:59

--- js/nfzm_contents.js
+++ js/nfzm_contents.js
@@ -1,7 +1,6 @@
  var body = $response.body
     .replace(/\"isview\":false/g, "\"isview\":true")
     .replace(/\"mode\":\"meterage\"/g, "\"mode\":\"free\"")
-    .replace(/\"cnt_mode\":\"meterage\"/g, "\"cnt_mode\":\"free\"")
     .replace(/\"is_original\":1/g, "\"is_original\":0")
     .replace(/\"people_magazine_content_id\":.*?,/g, "\"people_magazine_content_id\":0,")
     .replace(/\"magazine_content_id\":.*?,/g, "\"magazine_content_id\":0,")
