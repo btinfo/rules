efb07dcefb2356d4f15477e6da305bf432a2a3c4
btinfo <btchinainfo@gmail.com>
2021-10-20 23:53:07 +0800

    20211020@23:53

--- js/tianyaB.js
+++ js/tianyaB.js
@@ -1,3 +1,10 @@
 var body = $response.body
     .replace(/: <span/, ": 2099-12-31 <span");
 $done({ body });
+
+var body = $response.body;
+var obj = JSON.parse(body);
+obj.data.vipInfo.opType = 3;
+obj.data.vipInfo.isVas = 1;
+body = JSON.stringify(obj);
+$done({body});
