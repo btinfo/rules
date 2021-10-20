adf49a56e17b0b7661ce3741585c24556b2d8d54
btinfo <btchinainfo@gmail.com>
2021-10-20 23:58:01 +0800

    20211020@23:58

--- js/tianyaA.js
+++
@@ -1,3 +0,0 @@
-var body = $response.body
-    .replace(/\"isVipUser\":false/, "\"isVipUser\":true,\"expireTime\":4102415999000");
-$done({ body });
--- js/tianyaB.js
+++
@@ -1,6 +0,0 @@
-var body = $response.body;
-var obj = JSON.parse(body);
-obj.data.vipInfo.opType = 3;
-obj.data.vipInfo.isVas = 1;
-body = JSON.stringify(obj);
-$done({body});
