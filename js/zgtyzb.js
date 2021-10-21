1fed5bd5c8367a9bb0dd48f3807aa2cb84d2c9e3
btinfo <btchinainfo@gmail.com>
2021-10-22 02:09:35 +0800

    20211022@02:09

--- js/zgtyzb.js
+++ js/zgtyzb.js
@@ -1,9 +1,6 @@
 const path1 = "/room/get-room-info-v430";
-
 let obj = JSON.parse($response.body);
-
 if ($request.url.indexOf(path1) != -1){
 	obj.data.anchor["userHas"] = "1";	
 }
-
 $done({body: JSON.stringify(obj)});
