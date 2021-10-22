73b2a1f7627b2970469a879e67904836575f305f
btinfo <btchinainfo@gmail.com>
2021-10-23 02:00:20 +0800

    20211023@02:00

--- js/yunting.js
+++
@@ -1,3 +0,0 @@
-var body = $response.body
-  .replace(/\"isVip\":1/g, "\"isVip\":0");
-$done({ body });
--- js/zgtyzb.js
+++
@@ -1,6 +0,0 @@
-const path1 = "/room/get-room-info-v430";
-let obj = JSON.parse($response.body);
-if ($request.url.indexOf(path1) != -1){
-	obj.data.anchor["userHas"] = "1";	
-}
-$done({body: JSON.stringify(obj)});
