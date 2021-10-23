25eb79ae54cb769328d2c3a4d8a0e347f15159ae
btinfo <btchinainfo@gmail.com>
2021-10-24 02:17:42 +0800

    20211024@02:17

--- js/miguprogram.js
+++
@@ -1,7 +0,0 @@
-/*
-https://program-sc.miguvideo.com/program/v3/cont/
-*/
-var body = $response.body
-    .replace(/\"code\":\"WUDALIANSAIFUFEI01\"/g, "\"code\":\"FREE\"")
-    .replace(/\"code\":\"VIP\"/g, "\"code\":\"FREE\"");
-$done({ body });
