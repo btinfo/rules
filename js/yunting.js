affff52c34afc3d5b331ed2424b7c2a15ced7b4a
btinfo <btchinainfo@gmail.com>
2021-10-23 11:22:50 +0800

    20211023@11:22

--- js/yunting.js
+++ js/yunting.js
@@ -1,3 +1,2 @@
-var body = $response.body
-  .replace(/\"isVip\":1/g, "\"isVip\":0");
+var body = $response.body.replace(/\u0069\u0073\u0056\u0069\u0070\u0022\u003A\u0031/g, '\u0069\u0073\u0056\u0069\u0070\u0022\u003A\u0030');
 $done({ body });
