85cda11f78934df067b2e6547eb35bedf7896b6d
btinfo <btchinainfo@gmail.com>
2021-10-27 15:15:23 +0800

    20211027@15:15

--- js/wps.js
+++ js/wps.js
@@ -1,32 +1,31 @@
-var body = JSON.parse($response.body);
-var obj = {
-  exp: 0,
-  level: 3,
-  privilege: [
-    { spid: "data_recover", times: 0, expire_time: 1846256142 },
-    { spid: "ocr", times: 0, expire_time: 1846256142 },
-    { spid: "pdf2doc", times: 0, expire_time: 1846256142 },
-    { spid: "pdf_merge", times: 0, expire_time: 1846256142 },
-    { spid: "pdf_sign", times: 0, expire_time: 1846256142 },
-    { spid: "pdf_split", times: 0, expire_time: 1846256142 }
-  ],
-  result: "ok",
-  total_buy: 0,
-  total_cost: -30,
-  userid: body.userid,
-  vip: {
-    name: "\u8d85\u7ea7\u4f1a\u5458",
-    has_ad: 0,
-    memberid: 40,
-    expire_time: 1846256142,
-    enabled: [
-      { memberid: 40, name: "\u8d85\u7ea7\u4f1a\u5458", expire_time: 1846256142 },
-      { memberid: 20, name: "\u0057\u0050\u0053\u4f1a\u5458", expire_time: 1846256142 },
-      { memberid: 12, name: "\u7a3b\u58f3\u4f1a\u5458", expire_time: 1846256142 }
-    ]
-  },
-  wealth: 0,
-  expire_time: 1846256142
-};
+// WPS Office
+// ^https?:\/\/account\.wps\.cn\/api\/users requires-body=1,max-size=-1
+// hostname = account.wps.cn
 
-$done({ body: JSON.stringify(obj) });
+let obj = JSON.parse($response.body);
+obj.exp = "0";
+obj.level = "3";
+obj.privilege = [
+  { "spid": "data_recover", "times": 0, "expire_time": 1846256142 },
+  { "spid": "ocr", "times": 0, "expire_time": 1846256142 },
+  { "spid": "pdf2doc", "times": 0, "expire_time": 1846256142 },
+  { "spid": "pdf_merge", "times": 0, "expire_time": 1846256142 },
+  { "spid": "pdf_sign", "times": 0, "expire_time": 1846256142 },
+  { "spid": "pdf_split", "times": 0, "expire_time": 1846256142 }
+];
+obj.result = "ok";
+obj.total_buy = "0";
+obj.total_cost = "-30";
+obj.userid = "body.userid";
+obj.vip.name = "Super member";
+obj.vip.has_ad = "0";
+obj.vip.memberid = "40";
+obj.vip.expire_time = "1846256142";
+obj.vip.enabled = [
+  { "memberid": "40", "name": "Super member","expire_time": "1846256142" },
+  { "memberid": "20", "name": "WPS member", "expire_time": "1846256142" },
+  { "memberid": "12", "name": "Rice husk member", "expire_time": "1846256142" }
+];
+obj.wealth = "0";
+obj.expire_time = "1846256142";
+$done({body: JSON.stringify(obj)});
