ccd22615de6053e5e0c0cd21b3acff75bcbe041c
btinfo <btchinainfo@gmail.com>
2021-10-29 02:28:51 +0800

    20211029@02:28

--- js/ssl.js
+++ js/ssl.js
@@ -2,6 +2,10 @@
 
 var body = $response.body;
 var obj = JSON.parse(body);
+obj.Data.NoAds = 1;
+obj.Data.Days = 365;
+obj.Data.ASRTime = 30000;
+obj.Data.VIP = 1;
 obj.Data.VIP = 1;
 body = JSON.stringify(obj);
 $done({body});
