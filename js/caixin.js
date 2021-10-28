6fcdce2c8ac21da9b0ed29d7a5230eb2bf446950
btinfo <btchinainfo@gmail.com>
2021-10-28 23:27:53 +0800

    20211028@23:27

--- js/caixin.js
+++ js/caixin.js
@@ -36,7 +36,7 @@ $done({ body });
 "group":"A",
 "product_code":["QZSF","PRO","PRO_LITE","DATABASE_BASIC"],
 "fee_content_id":["100300000","101426574","101792465","77","401202509","401202158"]
-*******************************************************************************/
+
 var body = $response.body;
 var obj = JSON.parse(body);
   .replace(/\"isFree\":.*?,/g, "\"isFree\":1,")
@@ -48,3 +48,7 @@ obj.data.ios_ad_513 = [];
 obj.data.android_ad_513 = [];
 body = JSON.stringify(obj);
 $done({body});
+*/
+var body = $response.body.replace('var isPay = document', 'var isPay = 1;  #').replace('var articleProperties', 'var articleProperties2').replace(/nickname\":\".*?\"/g, 'nickname\":\"已解锁\"').replace('isFree\":\\d', 'isFree\":0').replace('aType\":\\d', 'aType\":0').replace('feeType\":\\d', 'feeType\":0')
+
+$done({ body });
