68c2c19cb4b0134338ac8ff8b4711e0fbc9d6098
btinfo <btchinainfo@gmail.com>
2021-10-28 15:22:16 +0800

    20211028@15:22

--- js/caixin.js
+++ js/caixin.js
@@ -1,10 +1,14 @@
 // ^https:\/\/mappsv5\.caixin\.com\/index_page_v5\/operate_index_page_1\.json
 
-var body = $response.body
+var body = $response.body;
+var obj = JSON.parse(body);
+obj.data.ios_ad_513 = [];
   //.replace('var isPay = document', 'var isPay = 1;  #')
   //.replace('var articleProperties', 'var articleProperties2')
   //.replace(/nickname\":\".*?\"/g, 'nickname\":\"已解锁\"')
-  .replace(/isFree\":\\d/g, 'isFree\":0')
-  .replace(/aType\":\\d/g, 'aType\":0')
-  //.replace(/feeType\":\\d/g, 'feeType\":0');
-$done({ body });
+  .replace(/\"article_type\":1/g, "\"article_type\":1")
+  .replace(/\"isFree\":1/g, "\"isFree\":0")
+  .replace(/\"aType\":2/g, "\"aType\":0")
+  .replace(/\"feeType\":1/g, "\"feeType\":0");
+body = JSON.stringify(obj);
+$done({body});
