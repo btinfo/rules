ecedc82ef2fc3817ae8f9f15b9811b2b4c1d4ba0
btinfo <btchinainfo@gmail.com>
2021-10-29 02:57:35 +0800

    20211029@02:57

--- js/ssl.js
+++ js/ssl.js
@@ -5,7 +5,6 @@ var obj = JSON.parse(body);
 obj.Data.NoAds = 1;
 obj.Data.Days = 365;
 obj.Data.ASRTime = 30000;
-obj.Data.VIP = 1;
-obj.Data.VIP = 1;
+obj.Data.VIP = "com.winat.recording.membership.yearly";
 body = JSON.stringify(obj);
 $done({body});
