43044fab2f783a9a156b2a60620ca7b9f77b4d1c
btinfo <btchinainfo@gmail.com>
2021-10-28 17:34:54 +0800

    20211028@17:34

--- js/caixin.js
+++ js/caixin.js
@@ -1,15 +1,30 @@
-// ^https:\/\/mappsv5\.caixin\.com\/index_page_v5\/operate_index_page_1\.json
+/*
+ ^https:\/\/mappsv5\.caixin\.com\/index_page_v5\/operate_index_page_1\.json
 
+"isFree":"1",
+"need_login":"0",
+"aType":"2",
+"from_channel":"16,22",
+"power":"1000,8",
+"channel_name":"\u5065\u5eb7",
+"icon_tag":"0",
+"source_id":"101792465",
+"app_id":"100",
+"top":"1",
+"ui_type":"2",
+"comment_num":"1",
+"pics":"https:\/\/imgs.caixin.com\/2021-10-27\/163533930676828.jpg",
+"group":"A",
+"product_code":["QZSF","PRO","PRO_LITE","DATABASE_BASIC"],
+"fee_content_id":["100300000","101426574","101792465","77","401202509","401202158"]
+*******************************************************************************/
 var body = $response.body;
 var obj = JSON.parse(body);
+  .replace(/\"isFree\":.*?,/g, "\"isFree\":1,")
+  .replace(/\"aType\":.*?,/g, "\"aType\":2,")
+  .replace(/\"power\":\".*?\"/g, "\"power\":\"\"")
+  .replace(/\"product_code\":\".*?\"/g, "\"product_code\":[]");
 obj.data.ios_ad_513 = [];
 obj.data.android_ad_513 = [];
-  //.replace('var isPay = document', 'var isPay = 1;  #')
-  //.replace('var articleProperties', 'var articleProperties2')
-  //.replace(/nickname\":\".*?\"/g, 'nickname\":\"已解锁\"')
-  //.replace(/\"article_type\":1/g, "\"article_type\":1")
-  //.replace(/\"isFree\":1/g, "\"isFree\":0")
-  //.replace(/\"aType\":2/g, "\"aType\":0")
-  //.replace(/\"feeType\":1/g, "\"feeType\":0");
 body = JSON.stringify(obj);
 $done({body});
