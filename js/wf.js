e5044505e6fbfb749747c86fe3d396ecf47b4160
btinfo <btchinainfo@gmail.com>
2021-10-30 14:08:58 +0800

    20211030@14:08

---
+++ js/wf.js
@@ -0,0 +1,8 @@
+// ^https?:\/\/api\.wfdata\.club\/v1\/user\/homePageInfo
+
+var body = $response.body;
+var obj = JSON.parse(body);
+obj.data.userBaseInfo.vipExpiryDate = "2029-12-31 23:59:59";
+obj.data.userBaseInfo.isVip = true;
+body = JSON.stringify(obj);
+$done({body});
